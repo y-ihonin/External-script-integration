8a20b30c95b558f7a5b8336d4c5e47c8d2edc881
y-ihonin <yevhenii.ihonin@coaxsoft.com>
2026-06-05 13:28:21 +0300

    initial setup

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import './style.css';
+
+const app = document.createElement('div');
+app.innerHTML = '<h1>Hello! I\'m working from one file.</h1>';
+document.body.appendChild(app);
---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+
+export default defineConfig({
+  plugins: [
+    cssInjectedByJsPlugin()
+  ],
+  build: {
+    minify: 'esbuild',
+    assetsDir: '',
+    rollupOptions: {
+      input: 'src/main.js',
+      output: {
+        entryFileNames: 'bundle.js',
+        assetFileNames: '[name].[ext]',
+        chunkFileNames: '[name].js',
+        manualChunks: undefined,
+      },
+    },
+  },
+});
